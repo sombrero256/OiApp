6c08de26a059a338d4cf3ac9aa4eb8b01ceada7e
Michael <marohrssen19@coe.edu>
2021-03-10 21:19:22 -0600

    Widget now has a background. In order for the widget to draw, it needs a shape to draw onto. Furthermore, the properties of the widget are defined in the widget.xml file, where the widget is declared after being included in the manifest. Finally, the oiwidget_layout file brings together the shape and additional elements to give us the thing that is drawn and placed on the screen. I need to study the code more to understand how everything works, but I understand the linking together of all of the xml now.

--- app/src/main/java/edu/coe/rohrssen/oiapp/OiWidget.kt
+++ app/src/main/java/edu/coe/rohrssen/oiapp/OiWidget.kt
@@ -1,6 +1,48 @@
 package edu.coe.rohrssen.oiapp
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import android.widget.Toast
 
 class OiWidget : AppWidgetProvider() {
+
+    override fun onUpdate(
+            context: Context,
+            appWidgetManager: AppWidgetManager, appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        for (appWidget in appWidgetIds) {
+            updateWidget(context, appWidgetManager, appWidget)
+        }
+    }
+
+    fun updateWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+
+        val updateViews = RemoteViews(
+                context.packageName, R.layout.oiwidget_layout
+        )
+        updateViews.setTextViewText(R.id.Name, "Myself")
+        updateViews.setTextViewText(R.id.Msg, "Oi, me!")
+
+        Toast.makeText(context, "I AM HERE!", Toast.LENGTH_LONG).show()
+
+        val intentUpdate = Intent(context, OiWidget::class.java)
+        intentUpdate.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+
+        val idArray = intArrayOf(appWidgetId)
+        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+        val pendingUpdate = PendingIntent.getBroadcast(
+                context, appWidgetId, intentUpdate,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+
+        updateViews.setOnClickPendingIntent(R.id.wholeWidget, pendingUpdate);
+
+        appWidgetManager.updateAppWidget(appWidgetId, updateViews)
+
+    }
+
 }
